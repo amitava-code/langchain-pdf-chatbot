00ee4e7c2adea4b0fe674f13f6edf71e9426b638
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-08 22:54:52 +0530

    refactor: improve PDF loading output using single mode and page_content access

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyPDFLoader
 
-loader = PyPDFLoader('./fullstack_guide.pdf')
+loader = PyPDFLoader('./fullstack_guide.pdf', mode="single")
 
 docs = loader.load()
 
-print(docs)
+print(docs[0].page_content)
 
 # model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
