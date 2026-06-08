5166e46b538dc74c0399dc744838a0301887cd00
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-08 22:46:41 +0530

    feat: integrate PyPDFLoader for PDF ingestion

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ load_dotenv()
 
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import PyPDFLoader
 
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+loader = PyPDFLoader('./fullstack_guide.pdf')
 
-response = model.invoke("what is full-stack ?")
+docs = loader.load()
 
-print(response.text)
+print(docs)
+
+# model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# response = model.invoke("what is full-stack ?")
+
+# print(response.text)
